8ec350a3a8352585491151220b96692a1edd7694
Aditya Vikram Pandey <adityavikrammvm@gmail.com>
2026-04-18 10:48:29 +0530

    [Aditya Vikram Pandey].Add - UC20 - Angular 17 frontend for the BACKEND Quantity Measurement .NET REST API.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: '/api/v1'
+  apiUrl: 'https://quantitymeasurementapp-esrs.onrender.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: '/api/v1'
+  apiUrl: 'https://quantitymeasurementapp-esrs.onrender.com/api'
 };
