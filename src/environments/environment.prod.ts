491f1f630c11495a8bcffd955a3310bb6f1301f1
Aditya Vikram Pandey <adityavikrammvm@gmail.com>
2026-04-18 11:18:26 +0530

    Correct API URL with v1 prefix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://quantitymeasurementapp-esrs.onrender.com/api'
+  apiUrl: 'https://quantitymeasurementapp-esrs.onrender.com/api/v1'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://quantitymeasurementapp-esrs.onrender.com/api'
+  apiUrl: 'https://quantitymeasurementapp-esrs.onrender.com'
 };
